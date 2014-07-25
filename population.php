3e3d05080d0d6a52d9d25c51807d5b3fa4fe6fb9
Henry Steinberg <henryjosephsteinberg@gmail.com>
2014-07-25 01:27:43 +0000

    i finally got the mysql to work

--- population.php
+++ population.php
@@ -1,32 +1,46 @@
-<html>
 <?php
 $city = $_GET['city'];
 ?>
+
 <h1>Welcome to Population Data Online!</h1>
-<?
-$cities = array(
-	'Philadelphia' => 1526006, 
-	'New York City' => 8405837, 
-	'Los Angeles' => 3862839, 
-	'Atlanta' => 450000, 
-	'Detroit' => 700000,
-	'San Diego' => 1338000, 
-);
 
-if (!$city || !isset($cities[$city])) {
-	print 'Please add a valid city name to your URL. Try <a href="population.php?city=Philadelphia">Philadelphia</a>.';
-} else {
-	print '<p>The population of <strong>' . $city . '</strong> is <strong>' . $cities[$city] . '</strong></p>';
+<?php
+
+$connection = mysql_connect('127.0.0.1', 'root', '');
+mysql_select_db('day-4-exercises');
+
+if (!$connection) {
+  die('Unable to connect: ' . mysql_errno());
+}
+
+if ($connection) {
+  $result = mysql_query('SELECT city_name, population FROM population');
+  // Another way to write it $result = mysql_query('SELECT city_name, population FROM population WHERE city_name ==' . $city);
+
+while ($row = mysql_fetch_array($result)) {
+	if ($city == $row['city_name']) {
+    echo 'The population of <strong>' . $row['city_name'] . '</strong> is '  . $row['population']  . '.<br>';
+	}
+  }
+}
+
+if (!$city) {
+	echo 'Please add a valid city name to your URL. Try <a href="population.php?city=Philadelphia">Philadelphia</a>.';
 }
 ?>
 
-<h1>Or select from the following:</h1>
+<h3>Or select from the following:</h3>
 
-<ul style="List-style-type: circle;">
-<?php 
-foreach ($cities as $name => $population) {
-	print '<li>what about <a href ="population.php?city='. $name . '">' . $name . '</a>?</li>'; 
-}
+<ol>
+<?php
+$result = mysql_query('SELECT city_name, population FROM population');
+while ($row = mysql_fetch_array($result)) {
+	print '<li>what about <a href ="population.php?city=' . $row['city_name'] . '">' . $row['city_name'] . '</a>?</li>';
+  }
 ?>
-</ul>
-</html>
+</ol>
+
+
+<?php
+mysql_close($connection);
+?>
